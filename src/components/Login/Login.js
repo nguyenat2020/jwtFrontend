df9f430e401e52a299bbf6a27519eaf4957ba04e
nguyenat2020 <great.expectations7760@gmail.com>
2023-08-30 18:02:31 +0900

    add navbar,  bootstrap library

---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <div className="login-container">
+            Login
+            <button className="btn btn-primary">Test App</button>
+        </div>
+    )
+}
+
+export default Login;
