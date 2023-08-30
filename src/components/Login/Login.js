62b7a8335ae4c0327188fb7b50aa89b31d5912b3
nguyenat2020 <great.expectations7760@gmail.com>
2023-08-30 19:06:12 +0900

    Login Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,29 @@
+import './Login.scss';
 const Login = () => {
     return (
-        <div className="login-container">
-            Login
-            <button className="btn btn-primary">Test App</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7">
+                        <div className='brand'>
+                            Great Expectations7760
+                        </div>
+                        <div className='detail'>
+                            Learning Fullstack Engineer
+                        </div>
+                    </div>
+                    <div className="content-right col-5 blue d-flex flex-column gap-3 py-3">
+                        <input className="form-control" type="text" placeholder='Email address or your phone number'/>
+                        <input className="form-control" type="password" placeholder="Password" />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>Forgot your password?</span>
+                        <hr/>
+                        <div className='text-center'>
+                        <button className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
